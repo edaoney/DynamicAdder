4eb2598af45882bd374be4b6e8e29f47d7a8855d
Eda Öney <oneeyyeda@gmail.com>
2025-07-28 19:16:43 +0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hesap Makinesi");
+
+Console.Write("Kaç sayı toplamak istiyorsunuz: ");
+int adet = int.Parse(Console.ReadLine());
+
+int toplam = 0;
+
+for (int i = 0; i < adet; i++)
+{
+    toplam += int.Parse(Console.ReadLine());
+    
+}
+Console.WriteLine($"Girilen sayıların toplamı: {toplam}");
